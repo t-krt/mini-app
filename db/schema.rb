f0518a9d4622df3740e38098e6b08e94937b7f50
t-krt <cest1la6vieg@gmail.com>
2019-05-16 23:07:34 +0900

    [Add]titleカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190516132308) do
+ActiveRecord::Schema.define(version: 20190516140228) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "post",       limit: 65535
     t.integer  "user_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "title",                    null: false
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
