3afde3aa20ffc46d54a661b0043d75d7f1992cf8
t-krt <cest1la6vieg@gmail.com>
2019-05-16 22:50:19 +0900

    [Add]ルーティングとアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(id: :DESC)
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  # def post_params
+  #   params.require(:post).permit(:post)
+  # end
 end
