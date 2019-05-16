d6e614a203c106b7d54630d2027b4f0015bc3d24
t-krt <cest1la6vieg@gmail.com>
2019-05-16 22:26:17 +0900

    [Add]postsテーブルの作成

--- db/migrate/20190516132308_create_posts.rb
+++ db/migrate/20190516132308_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-
+      t.text        :post
+      t.references  :user, foreign_key: :true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190516125809) do
+ActiveRecord::Schema.define(version: 20190516132308) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "post",       limit: 65535
+    t.integer  "user_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 20190516125809) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "users"
 end
