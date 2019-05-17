2e02a9e95af8f69e5a6ea598aa3ba99c44eec8f7
t-krt <cest1la6vieg@gmail.com>
2019-05-17 21:42:55 +0900

    [Add]マイページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def show
+    @name = current_user.name
+    @posts = Post.where( user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users
   resources :posts
+  resources :users, only: [:show]
 end
